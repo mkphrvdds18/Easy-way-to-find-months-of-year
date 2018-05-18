30e92cf4a5cc2f957d05cb392ee78b932b40e18a
mkphrvdds18 <29808555+mkphrvdds18@users.noreply.github.com>
2018-05-18 12:55:39 -0400

    Create month_names.py
    initial code , more to be added.

---
+++ month_names.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri May 18 11:45:18 2018
+
+@author: 
+- more codes will be added 
+
+"""
+import calendar
+print("Print all the months name of year: ")
+for month_number in range(1,13):
+    x = calendar.month_name[month_number]
+    print(x)
+
+print("Cool !")
